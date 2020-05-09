b883540a51c170e31633db3f9e89e66afc3da0a9
xnerhu <xnerhu@gmail.com>
2020-05-09 15:47:20 +0200

    refactor: remove same code

--- src/server/resolvers/home.ts
+++ src/server/resolvers/home.ts
@@ -1,22 +1,11 @@
-import { IHomePageData, IArticle } from '~/interfaces';
-import ArticleModel from '../models/article';
+import { IHomePageData } from '~/interfaces';
 import ArticleService from '../services/article';
 import { listFiles } from '../utils';
-
-const getArticles = async (): Promise<IArticle[]> => {
-  const items: IArticle[] = await ArticleModel.find()
-    .limit(9)
-    .sort({ _id: 1 })
-    .lean()
-    .exec();
-
-  return items.map((r) => ArticleService.format(r));
-};
 
 export default async (): Promise<IHomePageData> => {
   const [sliderItems, articles] = await Promise.all([
     listFiles('home-slider'),
-    getArticles(),
+    ArticleService.find({ limit: 9 }),
   ]);
 
   return {

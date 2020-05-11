f05b0d5e18a03ff983468e858a942eadbef300c8
xnerhu <xnerhu@gmail.com>
2020-05-11 18:10:27 +0200

    refactor: re-add dropdown

---
+++ src/renderer/components/Dropdown/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+import { Menu, MenuItem } from './Menu';
+import { setMenuPos, toggleMenu } from '~/renderer/utils/context-menu';
+import { StyledDropdown, Icon } from './style';
+
+export type IDropDownItem = {
+  id?: any;
+  name?: string;
+};
+
+interface Props {
+  items: IDropDownItem[];
+  placeholder?: string;
+  value?: any;
+  onChange?: (item: IDropDownItem) => void;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const Dropdown = React.forwardRef(
+  (
+    { placeholder, items, value, onChange, ...props }: Props,
+    setRef: React.MutableRefObject<HTMLDivElement>,
+  ) => {
+    const [expanded, setExpanded] = React.useState(false);
+    const ref = React.useRef<HTMLDivElement>();
+    const menuRef = React.useRef<HTMLDivElement>();
+
+    const selected = React.useMemo(() => items.find((r) => r.id === value), [
+      value,
+      items,
+    ]);
+
+    const onClick = React.useCallback(() => {
+      setExpanded(true);
+    }, []);
+
+    const onWindowClick = React.useCallback(() => {
+      setExpanded(false);
+    }, []);
+
+    React.useEffect(() => {
+      toggleMenu(menuRef.current, expanded);
+
+      if (expanded) {
+        setMenuPos(ref.current, menuRef.current);
+        window.addEventListener('click', onWindowClick);
+      } else {
+        window.removeEventListener('click', onWindowClick);
+      }
+    }, [expanded]);
+
+    React.useEffect(() => {
+      return () => window.removeEventListener('click', onWindowClick);
+    }, []);
+
+    const onItemClick = (item: IDropDownItem) => (e: React.MouseEvent) => {
+      if (onChange && value !== item.id) {
+        onChange(item);
+      }
+    };
+
+    return (
+      <StyledDropdown
+        ref={(r) => {
+          ref.current = r;
+          if (setRef) setRef.current = r;
+        }}
+        {...props}
+        onClick={onClick}
+      >
+        {selected?.name ?? placeholder}
+        <Icon className="drop-down-icon" />
+        <Menu ref={menuRef}>
+          {items.map(
+            (r) =>
+              r.id !== value && (
+                <MenuItem key={r.id} onClick={onItemClick(r)}>
+                  {r.name}
+                </MenuItem>
+              ),
+          )}
+        </Menu>
+      </StyledDropdown>
+    );
+  },
+);
+
+Dropdown.defaultProps = {
+  placeholder: 'Więcej',
+  items: [],
+} as Props;
---
+++ src/renderer/components/Dropdown/style.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+import { robotoMedium } from '~/renderer/mixins/typography';
+import { noUserSelect } from '~/renderer/mixins/user-selection';
+import { centerIcon } from '~/renderer/mixins/images';
+import { ICON_CHEVRON } from '~/renderer/constants/icons';
+
+export const StyledDropdown = styled.div`
+  height: 48px;
+  padding: 0px 16px;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.06);
+  will-change: background-color;
+  transition: 0.1s background-color;
+  ${robotoMedium()};
+  ${noUserSelect()};
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
+
+export const Icon = styled.div`
+  width: 20px;
+  height: 20px;
+  background-image: url(${ICON_CHEVRON});
+  margin-left: 4px;
+  transform: rotate(90deg);
+  ${centerIcon()};
+`;

7b8116a6d851e73657b502b8f98e7341171440de
xnerhu <xnerhu@gmail.com>
2020-05-11 13:22:59 +0200

    refactor: readd section component

---
+++ src/renderer/components/Section/index.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+import { CONTENT_WIDTH } from '~/renderer/constants/design';
+import { robotoMedium } from '~/renderer/mixins/typography';
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: ${CONTENT_WIDTH}px;
+  margin: 0 auto;
+
+  @media (max-width: ${CONTENT_WIDTH + 256}px) {
+    max-width: calc(100% - 128px);
+  }
+
+  @media (max-width: 767px) {
+    max-width: calc(100% - 64px);
+  }
+`;
+
+export const Background = styled.div`
+  width: 100%;
+  background-color: #fafafc;
+  padding-bottom: 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SectionTitle = styled.h5`
+  display: flex;
+  position: relative;
+  padding: 36px 0px;
+  ${robotoMedium()};
+`;

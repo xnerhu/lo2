847d2bf508656d510e5e8eda972d809b7e14fefa
xnerhu <xnerhu@gmail.com>
2020-05-26 16:25:48 +0200

    refactor: rewrite DraggableImage to hooks

--- src/renderer/components/ArticleEditor/DraggableImage/index.tsx
+++ src/renderer/components/ArticleEditor/DraggableImage/index.tsx
@@ -1,98 +1,83 @@
 import React from 'react';
 import { IPosition } from 'spatium';
 
-import { IS_BROWSER } from '~/renderer/constants/config';
 import { StyledDraggableImg, Img } from './style';
 
 interface Props {
   src: string;
   scale: number;
-  innerRef?: React.RefCallback<HTMLImageElement>;
   onChange?: (offset: IPosition) => void;
 }
 
-export class DraggableImg extends React.PureComponent<Props> {
-  private containerRef = React.createRef<HTMLDivElement>();
+const createTransform = ([x, y]: IPosition, scale: number) => {
+  return `translate(${x}px, ${y}px) scale(${scale})`;
+};
 
-  private imgRef: HTMLImageElement;
+export const DraggableImg = ({ src, scale, onChange }: Props) => {
+  const containerRef = React.useRef<HTMLDivElement>();
+  const imgRef = React.useRef<HTMLImageElement>();
 
-  private startPos: IPosition;
+  const startPos = React.useRef<IPosition>();
+  const offset = React.useRef<IPosition>([0, 0]);
 
-  private offset: IPosition = [0, 0];
+  const [active, setActive] = React.useState(false);
 
-  constructor(props: Props) {
-    super(props);
+  const update = React.useCallback(() => {
+    imgRef.current.style.transform = createTransform(offset.current, scale);
+  }, [scale]);
 
-    if (IS_BROWSER) {
-      this.removeListeners();
+  const onMouseDown = React.useCallback((e: React.MouseEvent) => {
+    const [x, y] = offset.current;
+
+    startPos.current = [e.pageX - x, e.pageY - y];
+    setActive(true);
+  }, []);
+
+  const onWindowMouseMove = React.useCallback(
+    (e: MouseEvent) => {
+      const containerRect = containerRef.current.getBoundingClientRect();
+      const imgRect = imgRef.current.getBoundingClientRect();
+      const [startX, startY] = startPos.current;
+
+      const xPos = (imgRect.width - containerRect.width) / 2;
+      const x = Math.max(Math.min(xPos, e.pageX - startX), -xPos);
+
+      const yPos = Math.ceil((imgRect.height - containerRect.height) / 2);
+      const y = Math.max(
+        Math.min(yPos, e.pageY - startY),
+        -yPos + 3 * (scale - 1),
+      );
+
+      offset.current = [x, y];
+
+      update();
+      onChange(offset.current);
+    },
+    [onChange, update, scale],
+  );
+
+  const onWindowMouseUp = React.useCallback(() => {
+    setActive(false);
+  }, []);
+
+  React.useEffect(() => {
+    if (active) {
+      window.addEventListener('mousemove', onWindowMouseMove);
+      window.addEventListener('mouseup', onWindowMouseUp);
+    } else {
+      window.removeEventListener('mousemove', onWindowMouseMove);
+      window.removeEventListener('mouseup', onWindowMouseUp);
     }
-  }
+  }, [active, onWindowMouseMove]);
 
-  private removeListeners() {
-    window.removeEventListener('mousemove', this.onWindowMouseMove);
-    window.removeEventListener('mouseup', this.onWindowMouseUp);
-  }
-
-  private onMouseDown = (e: React.MouseEvent) => {
-    const [x, y] = this.offset;
-
-    this.startPos = [e.pageX - x, e.pageY - y];
-
-    window.addEventListener('mousemove', this.onWindowMouseMove);
-    window.addEventListener('mouseup', this.onWindowMouseUp);
-  };
-
-  private onWindowMouseMove = (e: MouseEvent) => {
-    const { scale, onChange } = this.props;
-
-    const containerRect = this.containerRef.current.getBoundingClientRect();
-    const imgRect = this.imgRef.getBoundingClientRect();
-    const [startX, startY] = this.startPos;
-
-    const xPos = (imgRect.width - containerRect.width) / 2;
-    const x = Math.max(Math.min(xPos, e.pageX - startX), -xPos);
-
-    const yPos = Math.ceil((imgRect.height - containerRect.height) / 2);
-    const y = Math.max(
-      Math.min(yPos, e.pageY - startY),
-      -yPos + 3 * (scale - 1),
-    );
-
-    this.offset = [x, y];
-    this.update();
-
-    onChange(this.offset);
-  };
-
-  private onWindowMouseUp = () => {
-    this.removeListeners();
-  };
-
-  private update() {
-    const { scale } = this.props;
-    const [x, y] = this.offset;
-
-    this.imgRef.style.transform = `translate(${x}px, ${y}px) scale(${scale})`;
-  }
-
-  render() {
-    const { src, innerRef, scale } = this.props;
-
-    return (
-      <StyledDraggableImg
-        ref={this.containerRef}
-        onMouseDown={this.onMouseDown}
-      >
-        <Img
-          ref={(r) => {
-            this.imgRef = r;
-            // if (innerRef) innerRef(r);
-          }}
-          src={src}
-          draggable={false}
-          style={{ transform: `scale(${scale})` }}
-        />
-      </StyledDraggableImg>
-    );
-  }
-}
+  return (
+    <StyledDraggableImg ref={containerRef} onMouseDown={onMouseDown}>
+      <Img
+        ref={imgRef}
+        src={src}
+        draggable={false}
+        style={{ transform: createTransform(offset.current, scale) }}
+      />
+    </StyledDraggableImg>
+  );
+};
